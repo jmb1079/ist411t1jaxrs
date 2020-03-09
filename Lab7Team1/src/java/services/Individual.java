ce18bafbae42e07310845d5ae79f31b442e1b773
James Bristow <JamesBr@ashn.com>
2020-03-08 19:31:54 -0700

    Updated PUT method and removed deleted

--- Lab7Team1/src/java/services/Individual.java
+++ Lab7Team1/src/java/services/Individual.java
@@ -6,26 +6,23 @@
 package services;
 
 import java.io.ByteArrayInputStream;
-import java.io.File;
-import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.Arrays;
 import javax.json.Json;
-import javax.json.JsonArray;
 import javax.json.JsonObject;
-import javax.json.JsonObjectBuilder;
 import javax.json.JsonReader;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.UriInfo;
-import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PUT;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.core.Response;
 
 /**
  * REST Web Service
@@ -58,11 +55,13 @@ public class Individual {
 //    }
     @GET
     @Produces("text/html")
-    public String getHtml() throws IOException {
+    public String getHtml() throws IOException 
+    {
         return getAllIndividuals().toString();
     }
 
-    private JsonObject getAllIndividuals() {
+    private JsonObject getAllIndividuals() 
+    {
         String file = "IndividualData.json";
         try (JsonReader jsonReader = Json.createReader(getClass().getResource(file).openStream())) {
             JsonObject individualData = jsonReader.readObject();
@@ -84,28 +83,30 @@ public class Individual {
     public void updateIndividualDetails(String content) 
     {
         try (InputStream stream = new ByteArrayInputStream(content.getBytes())) {
+            Boolean passFormat = true;
             JsonReader reader = Json.createReader(stream);
             ArrayList<String> keys = new ArrayList<>(Arrays.asList("id", "fname", "lname", "state", "longitude", "latitude"));
-            for (String key : keys) 
+            for (String key : reader.readObject().keySet()) 
             {
-                if (true)//)!reader.readObject().keySet()) 
+                if (!keys.contains(key)) 
                 {
-                    
+                    passFormat = false;
                 }
-                if (true)//reader.readObject().containsKey())
-                {}
+            }
+            if(!reader.readObject().keySet().contains("id"))
+            {
+                passFormat = false;
             }
             JsonObject allIndividuals = getAllIndividuals();
+            //Here is where we need to filter and do update to all individuals for the individual changes;
 
         } catch (IOException ioEx) {
 
         }
     }
     
-    @DELETE
-    @Produces("text/html")
-    public String deleteIndividual(int id)
-    {
-        return "deleted";
+@DELETE
+public Response deleteConfiguration(){
+        return Response.status(Response.Status.OK).build();
     }
 }
